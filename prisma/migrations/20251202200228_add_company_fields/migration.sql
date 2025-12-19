75d5fa904bfd857219c967cdfe4530d2eb1f42a0
Sidiraos <raossidi@gmail.com>
2025-12-19 21:45:14 +0300

    feat: Reinitialize database schema by creating a new `init` migration and deleting previous migration files.

--- prisma/migrations/20251202200228_add_company_fields/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ADD COLUMN "companyDescription" TEXT;
-ALTER TABLE "User" ADD COLUMN "companyIndustry" TEXT;
-ALTER TABLE "User" ADD COLUMN "companyName" TEXT;
-ALTER TABLE "User" ADD COLUMN "companySize" TEXT;
-ALTER TABLE "User" ADD COLUMN "companyWebsite" TEXT;
